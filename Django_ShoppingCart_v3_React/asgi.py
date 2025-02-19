6d4e8453a7d1143a1aebf46d33652cd1e5b3f3c9
Jeffery Liu <a4264428@gmail.com>
2025-02-19 20:16:34 +0800

    modify file name to make it run

--- Django_ShoppingCart_v3_React/asgi.py
+++ Django_ShoppingCart_v3_React/asgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'Django_ShoppingCart_v2.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django_ShoppingCart_v3_React.settings")
 
 application = get_asgi_application()
--- Django_ShoppingCart_v3_React/wsgi.py
+++ Django_ShoppingCart_v3_React/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'Django_ShoppingCart_v2.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django_ShoppingCart_v3_React.settings")
 
 application = get_wsgi_application()
