fbc3a02ccc97160cd348a0ae317641fe0924c79e
jeffery <a4264428@gmail.com>
2025-02-26 17:35:27 +0800

    change file directory

--- backend/django_backend/settings.py
+++ backend/django_backend/settings.py
@@ -149,8 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = []
-# STATICFILES_DIRS = [BASE_DIR / "shop" / "static"]
+STATICFILES_DIRS = [BASE_DIR / "shop" / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
