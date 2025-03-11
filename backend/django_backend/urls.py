a1d2175cb4372d47cb595ea009ac73cfa9df712c
jeffery <a4264428@gmail.com>
2025-03-12 00:30:39 +0800

    add docker compose

--- backend/django_backend/urls.py
+++ backend/django_backend/urls.py
@@ -16,25 +16,24 @@ Including another URLconf
 from django.urls import path  # 引入 path 函數來定義路由
 from shop.views import *  # 匯入我們自定義的視圖模組，以便在路由中調用
 from rest_framework.routers import DefaultRouter
+
 urlpatterns = [
     # 後台登入頁面
-    path('admin/login/', admin_login, name='admin_login'),
+    path('login/', admin_login, name='admin_login'),
 
     # 後台登出功能
-    path('admin/logout/', admin_logout, name='admin_logout'),
-
-    path('admin/check_login/', check_admin_login, name='check_admin_login'),
-
+    path('logout/', admin_logout, name='admin_logout'),
+    path('check_login/', check_admin_login, name='check_admin_login'),
 ]
 
 router = DefaultRouter()
-router.register(r'admin/api', AdminViewSet)  # 註冊路由，這裡的 r 對應 URL 前綴
+router.register(r'admin', AdminViewSet)  # 註冊路由，這裡的 r 對應 URL 前綴
 urlpatterns += router.urls  # 自動生成 CRUD 對應的路由
 
 router = DefaultRouter()
-router.register(r'product/api',ProductViewSet)  # 註冊路由，這裡的 r 對應 URL 前綴
+router.register(r'product', ProductViewSet)  # 註冊路由，這裡的 r 對應 URL 前綴
 urlpatterns += router.urls  # 自動生成 CRUD 對應的路由
 
 router = DefaultRouter()
-router.register(r'footer/api', FooterViewSet)  # 註冊路由，這裡的 r 對應 URL 前綴
+router.register(r'footer', FooterViewSet)  # 註冊路由，這裡的 r 對應 URL 前綴
 urlpatterns += router.urls  # 自動生成 CRUD 對應的路由
